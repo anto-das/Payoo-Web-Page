318e13c8863bc65d8ef8f8ace3cb467033b14e9e
anto_das <antuvhai@gmail.com>
2024-09-19 17:51:11 +0600

    git-check

---
+++ js/login.js
@@ -0,0 +1,21 @@
+// step: 1
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    // step:2-pevent default behavior
+    event.preventDefault();
+    // step:3-get the phone number and pin number 
+    let phoneNumber = document.getElementById('phone-num').value;
+    let pinNumber = document.getElementById('pin-num').value;
+    console.log(phoneNumber, pinNumber);
+    // step:4-validate phone and pin
+    // this is temporary should do like this.
+    if(phoneNumber ==='1234' && pinNumber === '1234'){
+        console.log('you are logged in');
+        // step:5- allow
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or Pin')
+    }
+
+})
